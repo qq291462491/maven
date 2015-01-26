4b32ef2cb84251270b334dc68ad2a0778867182d
Igor Fedorenko <ifedorenko@apache.org>
2015-01-26 14:22:05 -0500

    .mvn/ for project specific jvm options and maven parameters
    Signed-off-by: Igor Fedorenko <ifedorenko@apache.org>

---
+++ apache-maven/src/bin/mvn-common.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# ----------------------------------------------------------------------------
+
+# TODO ideally, this should contain all logic common to mvn* shell scripts
+
+# traverses directory structure from process work directory to filesystem root
+# first directory with .mvn subdirectory is considered project base directory
+function find_maven_basedir {
+  local basedir=$(pwd)
+  local wdir=$(pwd)
+  while [ "$wdir" != '/' ]; do
+    wdir=$(cd $wdir/..; pwd)
+    if [[ -d "$wdir"/.mvn ]]; then
+      basedir=$wdir
+      break
+    fi
+  done
+  echo "${basedir}"
+}
+
+# concatenates all lines of a file
+function concat {
+  local file=$1
+  local result=
+  if [ -f "$file" ]; then
+    while read -r line; do
+      result="$result $line"
+    done < "$file"
+  fi
+  echo "$result"
+}
